30419d6560041240587f8b963599b368f9c06844
Sidney Crandall <sdncrandall5@gmail.com>
2021-04-05 14:44:55 -0500

    aded neww CSS and have cards rendering to DOM

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Employee.css";
 
-export const EmployeeCard = () => (
-    <section className="employee">
-        <h3 className="employee__name">Digby Hayes</h3>
-        <div className="employee__address">214 Princess Buttercup Way</div>
-    </section>
-)
+export const EmployeeCard = ({employee}) => { 
+    return (
+        <div className="card">
+            <div className="card-content">
+                <h3>Name: <span className="card-employeename">{employee.name}</span></h3>
+                <p>Address: {employee.address}</p>
+            </div>
+        </div>
+    )
+}
 
 
 //An exported function that represents the HTML of what will be multiple different employee. 
